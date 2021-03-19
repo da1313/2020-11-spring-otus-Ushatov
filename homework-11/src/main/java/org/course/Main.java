edadf7549040fc9b7879eb63573642f0124cfe80
Ushatov Mikhail <vv1313vv@yandex.ru>
2021-03-19 20:46:57 +0300

    Completed homework-11 remove unused dependencies

--- homework-11/src/main/java/org/course/Main.java
+++ homework-11/src/main/java/org/course/Main.java
@@ -1,23 +1,9 @@
 package org.course;
 
-//import com.github.cloudyrock.spring.v5.EnableMongock;
-import com.mongodb.client.result.UpdateResult;
-import org.bson.Document;
-import org.course.api.pojo.BookShort;
-import org.course.domain.*;
-import org.course.repositories.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.data.domain.PageRequest;
-import reactor.core.publisher.Flux;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
-//@EnableMongock
 @SpringBootApplication
 public class Main {
 
